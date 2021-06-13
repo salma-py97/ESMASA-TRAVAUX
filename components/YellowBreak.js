9bbb2121d1bbfb03e86a0544876fc655f2a751c5
Salma EL BARMAKI <elbarmaki.salma@gmail.com>
2021-06-13 07:36:25 +0100

    changed Hero Section bg-image

--- components/YellowBreak.js
+++ components/YellowBreak.js
@@ -6,8 +6,8 @@ const YellowBreak = () => {
   return (
     <div className={`bg-warning text-white text-start p-5 d-flex justify-content-center align-items-center ${bg}`}>
       <div className="py-5 d-flex justify-content-between align-items-start flex-column" >
-      <h2>IF YOU HAVE A PROJECT, WE WOULD TO HELP YOU OUT</h2>
-      <p className="lead mt-3 mb-4">TRUST US WITH YOU IDEAS AND WE WILL BRING THEM TO LIFE</p>
+      <h2>IF YOU HAVE A PROJECT, WE WOULD LOVE TO HELP YOU OUT</h2>
+      <p className="lead mt-3 mb-4">TRUST US WITH YOUR IDEAS, AND WE WILL BRING THEM TO LIFE</p>
       <button className={` mr-auto ${btn} ${btn__outline} ${btn__wide}`}>CONTACT US</button>
     </div>
   </div>
---
+++ pages/sécurité.js
@@ -0,0 +1,16 @@
+
+
+
+
+
+
+const Sécurité = () => {
+
+  return (
+    <h1>
+      SECURITE
+    </h1>
+  )
+}
+
+export default Sécurité
