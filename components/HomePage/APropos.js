d85f7d5912252fbf2531c524120a9bf135789fa4
Salma EL BARMAKI <elbarmaki.salma@gmail.com>
2021-06-15 13:20:48 +0100

    fixed display in About section in homepage

--- components/HomePage/APropos.js
+++ components/HomePage/APropos.js
@@ -6,14 +6,14 @@ const APropos = () => {
   const {bg, hr, desc, title} = styles
   const {btn, btn__yellow, btn__dark} = buttonStyles
   return (
-    <div className={`py-5 ${bg}`}>
-      <div className="row d-flex justify-content-around align-item-center container">
-        <div className="col-md-5 d-flex flex-column justify-content-center align-items-start">
+    <div className={`py-5 d-flex justify-content-center align-items-center ${bg}`}>
+      <div className="row d-flex justify-content-between align-items-start container">
+        <div className="col-md-4 d-flex flex-column justify-content-center align-items-start">
           <h2 className={title} >À PROPOS</h2>
-          <hr className={`border mx-0 mb-5 ${hr}`} />
+          <hr className={`mx-0 mb-5 ${hr}`} />
           <p className={`lead ${desc}`}>WE ARE THE LEADERS IN THE CONSTRUCTION INDUSTRY!</p>
         </div>
-        <div className="col-md-5 d-flex flex-column justify-content-center align-items-start">
+        <div className="col-md-6 d-flex flex-column justify-content-center align-items-start">
           <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vel eveniet accusamus veniam cupiditate optio ullam exercitationem ex facilis. In nesciunt atque fugit? Vitae repudiandae iste ipsa eos voluptate magni at fugiat neque nulla, deleniti rerum eaque tempora tenetur necessitatibus perspiciatis nemo omnis aliquam consectetur ut accusantium hic. Minus, eveniet labore!</p>
           <div className="d-flex">
           <Link href="/contact">
