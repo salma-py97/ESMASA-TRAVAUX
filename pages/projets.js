301536c8f8bca644c798924680759771ea116e98
Salma EL BARMAKI <elbarmaki.salma@gmail.com>
2021-06-16 05:27:45 +0100

    added Stats section in projects page

--- pages/projets.js
+++ pages/projets.js
@@ -1,11 +1,13 @@
 import HeroSection from '../components/ProjectsPage/HeroSection'
 import Process from '../components/ProjectsPage/Process'
+import Stats from '../components/ProjectsPage/Stats'
 
 const projets = () => {
   return (
     <>
       <HeroSection />
       <Process />
+      <Stats />
     </>
   )
 }
