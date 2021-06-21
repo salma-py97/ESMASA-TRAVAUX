5c4d5290fa12c479f36dfa737cf67cc3146241ac
Salma EL BARMAKI <elbarmaki.salma@gmail.com>
2021-06-21 21:35:58 +0100

    changed values component layout

--- components/HomePage/APropos.js
+++ components/HomePage/APropos.js
@@ -12,6 +12,7 @@ const APropos = () => {
           <h2 className={title} >À PROPOS</h2>
           <hr className={`mx-0 mb-5 ${hr}`} />
           <p className={`lead text-uppercase ${desc}`}>VOTRE VISION. NOTRE métier.</p>
+          {/* <p className={`${desc}`}>Planification effective, conception efficiente et construction de qualité</p> */}
         </div>
         <div className="col-md-6 d-flex flex-column justify-content-center align-items-start">
           <p>Esmasa a été fondée en 2008 avec pour finalité de ...... Avec plus de 20 ans d'expérience professionnelle dans le secteur de la construction, nous avons entrepris de nombreux projets exigeants et accumulé des compétences, du savoir-faire et de l'expérience en matière de conception et de construction, de services de gestion de projets, de métiers du bâtiment et de travaux d'ingénierie. Aujourd'hui, Esmasa offre des services de gestion et coordonne des métiers spécialisés pour des projets industriels/commerciaux. Esmasa est fière de sa gestion moderne et de son personnel technique qualifié et professionnel, utilisant des systèmes organisationnels efficaces pour diversifier ses activités liées au domaine de la construction qui couvrent toute la gamme des services auxiliaires.</p>
--- components/HomePage/Mission.js
+++ components/HomePage/Mission.js
@@ -1,20 +1,42 @@
-import styles from '../../styles/HomePage/Mission.module.css'
+import styles from "../../styles/HomePage/Mission.module.css"
 
 const Mission = () => {
-  const {bg, hr, inner_bg} = styles
-  return (
-    <div className={`d-flex flex-column justify-content-center align-items-center text-center py-5 mx-2 ${bg}`}>
-      <div className={`p-md-5 p-1 mx-md-5 d-flex-justify-content-center align-items-center container ${inner_bg}`}>
-        <div className="my-5 mt-md-0">
-          <h2>Notre Mission</h2>
-          <hr className={hr} />
-        </div>
+	const { bg, hr, inner_bg } = styles
+	return (
+		<div
+			className={`d-flex flex-column justify-content-center align-items-center text-center py-5 mx-2 ${bg}`}
+		>
+			<div
+				className={`p-md-5 p-1 mx-md-5 d-flex-justify-content-center align-items-center container ${inner_bg}`}
+			>
+				<div className="my-5 mt-md-0">
+					<h2>Notre Mission</h2>
+					<hr className={hr} />
+				</div>
 
-        <p className="lead px-md-5 px-3 mb-5 mb-md-0">Garantir la meilleure qualité du travail et du service à la clientèle et maintenir le plus haut niveau de professionnalisme, d'honnêteté et d'équité dans nos relations avec nos clients, nos employés et nos fournisseurs. <br /><br />
-        Nous nous engageons à se développer en fournissant continuellement des produits, des services et des solutions simples et efficaces aux marchés que nous servons déjà, et se lancer dans de nouveaux domaines qui s'appuient sur nos compétences et les intérêts de nos clients.</p>
-      </div>
-    </div>
-  )
+				<p className="lead px-md-5 px-3 mb-5 mb-md-0">
+					Garantir la meilleure qualité du travail, des produits et des services
+					à nos clients en assurant le plus haut niveau de sécurité, d'intégrité
+					et d'innovation, tout en respectant l'environnement et en favorisant
+					le développement de nos employés et de nos communautés.
+					<br />
+					<br />
+					Maintenir le plus haut niveau de professionnalisme, d'honnêteté et
+					d'équité dans nos relations avec nos clients, nos employés et nos
+					fournisseurs afin d'être le prestataire de choix pour les services de
+					construction en surpassant les exigences et les attentes des clients
+					et en apportant une valeur inégalée.
+					<br />
+					<br />
+					Nous nous engageons à se développer en fournissant continuellement des
+					produits, des services et des solutions simples et efficaces aux
+					marchés que nous servons déjà, et se lancer dans de nouveaux domaines
+					qui s'appuient sur nos compétences et les intérêts de nos clients.
+				</p>
+			</div>
+		</div>
+	)
 }
+// Garantir la meilleure qualité du travail et du service aux clients et maintenir le plus haut niveau de professionnalisme, d'honnêteté et d'équité dans nos relations avec nos clients, nos employés et nos fournisseurs afin d'être le prestataire de choix pour les services de construction en surpassant les exigences des clients et en apportant une valeur inégalée.
 
 export default Mission
