8e93fe05bd02674b76abf38ad7ffc85a4d0de7b5
Salma EL BARMAKI <elbarmaki.salma@gmail.com>
2021-06-24 14:23:27 +0100

    changed content

--- components/HomePage/Valeurs.js
+++ components/HomePage/Valeurs.js
@@ -6,7 +6,7 @@ const Valeurs = () => {
     <div className={`py-5 ${bg}`}>
       <div className={`container py-4 ${wrapper}`}>
         <h2 className={`text-center mb-4 ${title}`}>Nos Valeurs <hr className={hr} /></h2>
-        <p className="lead text-center mb-5 px-md-4">Ce sont les caractéristiques qui définissent qui nous sommes et comment nous abordons notre travail.</p>
+        <p className="lead text-center mb-5 px-md-4">Les caractéristiques qui définissent qui nous sommes et comment nous abordons notre travail.</p>
         <div className={`row justify-content-around align-items-center ${valeurs}`}>
           <div className="col-10 col-sm-6 col-md-4 col-lg-3 p-3 text-dark d-flex justify-content-center align-items-center">
             {/* <div className={`d-flex justify-content-center align-items-center flex-column py-4 px-3 text-center h-100 w-100 ${box}`}> */}
