fb305d802583a1425515b358b11940a07104bea0
Salma EL BARMAKI <elbarmaki.salma@gmail.com>
2021-10-08 14:23:47 +0100

    changed content on projects page

--- components/ProjectsPage/data.js
+++ components/ProjectsPage/data.js
@@ -1,20 +1,20 @@
 export const projectOne = {
   reverse: false,
   src: "https://i.ibb.co/z5pzGhN/1-3.jpg",
-  client: "AFRIQUIA GAZ",
-  type: "Hangar",
-  lieu: "Casablanca",
-  date: "4 mois",
-  prestations: ["Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat, numquam!", "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat", "Lorem ipsum dolor sit amet consectetur adipisicing elit!", "Lorem ipsum dolor sit amet consectetur adipisicing", "Lorem ipsum dolor sit amet consectetur"]
+  client: "Amendis",
+  type: "Résaux d'assainissement",
+  lieu: "Tanger",
+  date: "1 an",
+  prestations: ["Térassement et pose de canalisations en béton armé", "Térassement et pose de canalisations en fonte ductile", "Térassement et pose de canalisations en PVC", "Térassement et pose de canalisations en PP Polypropylène", "Térassement et pose de canalisations pour drainage", "Réalisation des ouvrages en béton armé tel que: dalots, déversoirs d'orages, regards préfabriqués ou coulés sur place, réhabilitations des ouvrage annexes", "Travaux de réfections de voirie"]
 }
 export const projectTwo = {
   reverse: true,
   src: "https://i.ibb.co/z5pzGhN/1-3.jpg",
-  client: "AFRIQUIA GAZ",
-  type: "Hangar",
-  lieu: "Casablanca",
-  date: "4 mois",
-  prestations: ["Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat, numquam!", "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat", "Lorem ipsum dolor sit amet consectetur adipisicing elit!", "Lorem ipsum dolor sit amet consectetur adipisicing", "Lorem ipsum dolor sit amet consectetur"]
+  client: "Redal",
+  type: "Alimentation en eau potable",
+  lieu: "Salé",
+  date: "6 mois",
+  prestations: ["Travaux de térassement, fourniture, transport et pose d'une conduite en PVC", "Travaux de térassement, fourniture, transport et pose d'une conduite en fonte ductile", "Travaux de térassement, fourniture, transport et pose de tuyaux en PVC", "Fourniture, transport et pose des pièces spéciales de raccords", "Fourniture, transport et pose d'un réducteur de pression", "Fourniture, transport et pose d'un débitmètre électromagnétique", "Construction des regards en béton armé pour abriter les équipements hydromécaniquesà poser"]
 }
 export const projectThree = {
   reverse: false,
