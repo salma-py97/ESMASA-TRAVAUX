2302540cd7c1ab74e39730ef588ab42f81df9763
Salma EL BARMAKI <elbarmaki.salma@gmail.com>
2021-10-28 21:16:59 +0100

    removed form action

--- pages/contact.js
+++ pages/contact.js
@@ -129,7 +129,6 @@ const contact = () => {
 						className={`${contact_form} row`}
 						name="contact"
 						method="POST"
-						action="mailto:contact@esmasatravaux.com"
 						// data-netlify="true"
 					>
 						<div className={`${form_field} col-lg-6`}>
