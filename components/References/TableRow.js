29bb798211403e396f588bb470c8a2d339eb036f
Salma EL BARMAKI <elbarmaki.salma@gmail.com>
2021-11-10 15:45:10 +0100

    fixed column width in table

--- components/References/TableRow.js
+++ components/References/TableRow.js
@@ -2,14 +2,14 @@ import styles from '../../styles/ReferencesPage/styles.module.css'
 
 const TableRow = ({client, projet, durée, année}) => {
 
-  const {tr} = styles
+  const {tr, td} = styles
 
   return (
     <tr className={tr}>
-      <td className="text-nowrap text-center">{année}</td>
-      <td className="text-nowrap text-center">{client}</td>
-      <td className="text-capitalize">{projet}</td>
-      <td className="text-nowrap text-center">{durée}</td>
+      <td className={`text-nowrap text-center ${td}`}>{année}</td>
+      <td className={`text-center ${td}`}>{client}</td>
+      <td className={`text-capitalize ${td}`}>{projet}</td>
+      <td className={`text-nowrap text-center ${td}`}>{durée}</td>
     </tr>
   )
 }
