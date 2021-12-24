6ff9c64e23a16976eadf8ed074e78196588f6e55
Salma EL BARMAKI <elbarmaki.salma@gmail.com>
2021-12-24 19:41:42 +0100

    bold on hover

--- components/ServicesPage/Break.js
+++ components/ServicesPage/Break.js
@@ -3,7 +3,7 @@ import Link from 'next/link'
 
 import buttonStyles from '../../styles/LayoutStyles/Button.module.css'
 const YellowBreak = () => {
-  const {bg, hr} = styles
+  const {bg, hr, button} = styles
   const {btn, btn__outline, btn__wide} = buttonStyles
   return (
     <div className={`text-white text-center d-flex justify-content-center align-items-center py-5 ${bg}`}>
@@ -13,10 +13,10 @@ const YellowBreak = () => {
       <h4 className="text-center mt-4">Consultez les projets qu'on a réalisé</h4>
       <div className='d-flex flex-wrap align-items-center justify-content-center'> 
         <Link href="/projets" >
-          <button className={`mt-4 ${btn} ${btn__outline} ${btn__wide}`}>NOS PROJETS</button>
+          <button className={`mt-4 ${button} ${btn} ${btn__outline} ${btn__wide}`}>NOS PROJETS</button>
         </Link>
         <Link href="/contact" >
-          <button className={`mt-4 ${btn} ${btn__outline} ${btn__wide}`}>CONTACTEZ NOUS</button>
+          <button className={`mt-4 ${button} ${btn} ${btn__outline} ${btn__wide}`}>CONTACTEZ NOUS</button>
         </Link>
       </div>
     </div>
