077a030d7715739458befe261bf5dccc62164aa7
Salma EL BARMAKI <elbarmaki.salma@gmail.com>
2021-12-28 23:09:29 +0100

    deleted some keywords from Meta

--- components/Meta.js
+++ components/Meta.js
@@ -17,8 +17,8 @@ const Meta = ({title, keywords, description}) => {
 }
 
 Meta.defaultProps = {
-    title: 'Esmasa Travaux',
-    keywords: "Esmasa, Esmasa Travaux, esmasa travaux, construction, esmasa construction maroc, génie civil, construction maroc, construction casa, LA RÉNOVATION DE BÂTIMENTS DURABLES, construction casablanca, construction mohammedia, génie civil maroc, génie civil casa, génie civil casablanca, génie civil mohammedia, Terrassement, Remblai, Béton de Fondation, Béton d’élévation, Maçonnerie, Toiture en Béton, Toiture en autoportante, Etanchéité, Revêtement des sols et murs, Menuiseries, Travaux Enduisage, motif et peinture, Travaux d’électricité intérieure, Travaux des descentes d’eau pluviale, Plomberie sanitaire-Protection Incendie, Climatisation- VMC, VRD - Aménagements extérieur, Travaux d’aménagement des espaces verts, Travaux d’éclairage public, Travaux d’assainissement pour drainage d’eaux pluviales, BTP, BTP maroc, BTP casa, BTP casablanca, BTP mohammedia, BTP redal, BTP amendis, Résaux d'assainissement, Alimentation en eau potable, rénovation, aménagement, alimentation en eau potable, installation en eau potable",
+    title: 'ESMASA Travaux',
+    keywords: "Esmasa, Esmasa Travaux, esmasa travaux, esmasatravaux, ESMASA, ESMASA Travaux, esmasa construction maroc",
     description: 'LEADER DANS LA CONSTRUCTION ET LA RÉNOVATION DE BÂTIMENTS DURABLES'
 }
 
